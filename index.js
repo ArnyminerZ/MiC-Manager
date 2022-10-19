755088a8f13b8148253d7d232b8b0efcd3a57377
Arnau Mora <arnyminer.z@gmail.com>
2022-10-19 15:17:34 +0200

    :sparkles: Added `/v1/user/change_password` for initial password
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import reqIp from 'request-ip';
-import {errorResponse} from './src/response.js';
+import {errorResponse, successResponse} from './src/response.js';
 import {check as dbCheck} from './src/database.js';
-import {login, PasswordlessUserException} from "./src/auth.js";
+import {changePassword, login, PasswordlessUserException} from "./src/auth.js";
 
 dotenv.config();
 
@@ -27,6 +27,8 @@ const app = express();
 
 // Middleware
 app.use(reqIp.mw());
+app.use(express.json({strict: false}));
+app.use(express.urlencoded())
 
 app.get('/v1/user/auth', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
@@ -53,6 +55,29 @@ app.get('/v1/user/auth', async (req, res) => {
             res.status(500).json({success: false, error: 'unknown', errorData: JSON.stringify(e)});
         }
     }
+});
+app.post('/v1/user/change_password', async (req, res) => {
+    const body = req.body;
+    /**
+     * @type {string|null}
+     */
+    const dni = body['dni'];
+    /**
+     * @type {string|null}
+     */
+    const password = body['password'];
+    /**
+     * @type {string|null}
+     */
+    const apiKey = req.headers['apiKey']
+
+    console.log('dni:', dni, 'password:', password, 'body:', req.body);
+    if (dni == null || password == null)
+        return res.status(400).json(errorResponse('missing-parameters'));
+    const passwordChanged = await changePassword(dni, password, apiKey);
+    if (passwordChanged)
+        return res.status(200).json(successResponse());
+    return res.status(500).json({error: 'work in progress'});
 });
 
 app.listen(HTTP_PORT, () => console.info(`🖥️ Listening for requests on http://localhost:${HTTP_PORT}`));
