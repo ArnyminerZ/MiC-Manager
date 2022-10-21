eebb424ff9a4092e81e750b4bf282674c6c34d29
Arnau Mora <arnyminer.z@gmail.com>
2022-10-21 18:55:59 +0200

    :sparkles: Added `tables` and `attending` to `getEvents`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
+import dateFormat from 'dateformat';
+
 import {query as dbQuery} from './database.js';
 import {UserNotFoundException} from "./exceptions.js";
+
+/**
+ * Returns the given string as a formatted date following yyyy-MM-dd.
+ * @author Arnau Mora
+ * @since 20221021
+ * @param {string} date The date to parse.
+ * @return {string}
+ */
+const formatDayDate = (date) => dateFormat(new Date(date), 'yyyy-MM-dd');
 
 export const getUserData = async (socioId) => {
     const sql = `SELECT *
@@ -16,6 +27,7 @@ export const getUserData = async (socioId) => {
         postalCode: row['idCodPostal'],
         dni: row['Dni'],
         born: row['FecNacimiento'],
+        registration: row['FecAlta'],
         workPhone: row['TlfParticular'],
         homePhone: row['TlfTrabajo'],
         mobilePhone: row['TlfMovil'],
@@ -30,32 +42,106 @@ export const getUserData = async (socioId) => {
                 locked: row['bRodaNegros'],
             },
         },
+        trebuchet: !row['bCarnetAvancarga'] ? null : {
+            shoots: row['bDisparaAvancarga'],
+            obtained: formatDayDate(row['FecExpedicionAvancarga']),
+            expires: formatDayDate(row['FecCaducidadAvancarga']),
+        },
     };
 };
+
+/**
+ * @typedef {Object} TableData
+ * @property {number} responsible
+ * @property {number} members
+ */
+
+/**
+ * @typedef {Object} EventData
+ * @property {number} id
+ * @property {string} displayName
+ * @property {string} date
+ * @property {Object?} menu
+ * @property {string?} contact
+ * @property {string?} description
+ * @property {number} category
+ * @property {number[]} attending
+ * @property {TableData?} table
+ */
 
 /**
  * Gets a list of all the available events.
  * @author Arnau Mora
  * @since 20221021
- * @return {Promise<[{id:number,displayName:string,date:string,menu:Object?,contact:string?,description:string?,category:number}]>}
+ * @return {Promise<EventData[]>}
  */
 export const getEvents = async () => {
-    const sql = `SELECT *
-                 FROM GesTro.dbo.mEvents;`;
+    const sql = `SELECT mEvents.*,
+                        mA.Person      as AttPerson,
+                        mT.Responsible as TableResponsible,
+                        mTP.Person     as TableMember
+                 FROM GesTro.dbo.mEvents
+                          LEFT JOIN mAssistance mA ON mEvents.id = mA.Event
+                          LEFT JOIN mTables mT on mEvents.id = mT.Event
+                          LEFT JOIN mTablesPeople mTP on mT.Id = mTP.TableId;`;
     const result = await dbQuery(sql);
+    /**
+     * @type {EventData[]}
+     */
     let builder = [];
+    /**
+     * @type {Map<number, number[]>}
+     */
+    let attendants = new Map();
+    /**
+     * @type {Map<number, {responsible:number,members:number[]}>}
+     */
+    let tables = new Map();
     const size = result.rowsAffected[0];
+    /**
+     * @type {{'mEvents.id':number,DisplayName:string,Date:string,Menu:string?,Contact:string?,Description:string?,Category:number,'mA.Id':number?,'mA.Event':number?,'mA.Person':number?,'mT.Id':number?,Responsible:number?,'mT.Event':number?,'mTP.Id':number?,'mTP.Person':number?,'TableId':number?}}
+     */
+    const rows = result.recordset;
     for (let c = 0; c < size; c++) {
-        const row = result.recordset[c];
-        builder.push({
-            id: row['id'],
-            displayName: row['DisplayName'],
-            date: row['Date'],
-            menu: row['Menu'],
-            contact: row['Contact'],
-            description: row['Description'],
-            category: row['Category'],
-        })
+        const row = rows[c];
+        const eventId = row['id'];
+
+        if (builder.hasOwnProperty(eventId)) {
+            if (row['AttPerson'] != null) {
+                const a = attendants[eventId] ?? [];
+                a.push(row['AttPerson']);
+                attendants[eventId] = a;
+            }
+
+            if (row['TableMember'] != null) {
+                const t = tables[eventId] ?? {responsible: -1, members: []};
+                t.members.push(row['TableMember']);
+                tables[eventId] = t;
+            }
+        } else {
+            builder.push({
+                id: row['id'],
+                displayName: row['DisplayName'],
+                date: row['Date'],
+                menu: JSON.parse(row['Menu']),
+                contact: row['Contact'],
+                description: row['Description'],
+                category: row['Category'],
+                attending: [],
+            });
+            const attendingPerson = row['AttPerson'];
+            if (!!attendingPerson)
+                attendants[eventId] = [attendingPerson];
+
+            const tableResponsible = row['TableResponsible'];
+            const tableMember = row['TableMember'];
+            if (!!tableResponsible || !!tableMember)
+                tables[eventId] = {responsible: tableResponsible, members: [tableMember]};
+        }
     }
-    return builder;
+    return builder.map((ev) => {
+        ev.attending = attendants[ev.id];
+        ev.table = tables[ev.id];
+        return ev;
+    });
 }
