e24480f7a0ec9e70e71d32d140cebd7141f83e9b
Arnau Mora <arnyminer.z@gmail.com>
2022-10-25 12:00:51 +0200

    :sparkles: Implemented `hasPermission`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/permissions.js
+++ src/permissions.js
@@ -1,3 +1,24 @@
-export const hasPermission = (userId, permission) => {
+import {query as dbQuery} from './database.js';
 
+/**
+ * @typedef {("view-user-data")} Permission
+ */
+
+/**
+ * Checks if a given user has a given permission.
+ * @author Arnau Mora
+ * @since 20221025
+ * @param {number} userId The id of the user to check for.
+ * @param {Permission} permission The permission name to check.
+ * @return {Promise<boolean>}
+ */
+export const hasPermission = async (userId, permission) => {
+    const query = await dbQuery(`SELECT mUsers.Role as UserRole, mP.Name as Permission
+                                 FROM GesTro.dbo.mUsers
+                                          LEFT JOIN mRolesPermissions mRP ON mUsers.Role = mRP.Role
+                                          LEFT JOIN mRoles mR ON mRP.Role = mR.Id
+                                          LEFT JOIN mPermissions mP on mRP.Permission = mP.Id
+                                 WHERE mUsers.Id = ${userId}
+                                   AND mR.Name = '${permission}';`);
+    return query.rowsAffected[0] > 0;
 };
