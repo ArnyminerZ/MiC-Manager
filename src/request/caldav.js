96bf8a6c7771da5ff182c4c9c59c39fb96c92070
Arnau Mora <arnyminer.z@gmail.com>
2022-11-04 23:58:49 +0100

    :sparkles: Added vCard fetching
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/request/caldav.js
+++ src/request/caldav.js
@@ -1,5 +1,6 @@
 import tsdav from 'tsdav';
 import dotenv from 'dotenv';
+import {parseCards} from "../parser/vcard.js";
 
 const {createDAVClient, DAVObject, DAVCollection} = tsdav;
 
@@ -11,7 +12,19 @@ let client,
     /** @type {DAVObject[]} */
     vCards;
 
-export const createClient = async () => {
+/**
+ * Fetches the cards data from the server, and stores it locally.
+ * @author Arnau Mora
+ * @since 20221104
+ * @return {Promise<void>}
+ */
+export const fetchCards = async () => {
+    addressBook = (await client.fetchAddressBooks()).find(v => v.url === process.env.CALDAV_AB_URL);
+    if (addressBook == null) throw Error('Could not find an address book with the url: ' + process.env.CALDAV_AB_URL);
+    vCards = await client.fetchVCards({addressBook: addressBook});
+}
+
+export const createClient = async (debug = process.env.DEBUG) => {
     if (client == null)
         client = await createDAVClient({
             serverUrl: process.env.CALDAV_HOSTNAME,
@@ -22,9 +35,25 @@ export const createClient = async () => {
             authMethod: 'Basic',
             defaultAccountType: 'carddav',
         });
-    addressBook = (await client.fetchAddressBooks()).find(v => v.url === process.env.CALDAV_AB_URL);
-    if (addressBook == null) throw Error('Could not find an address book with the url: ' + process.env.CALDAV_AB_URL);
-    vCards = await client.fetchVCards({addressBook: addressBook});
-    if (process.env.DEBUG === 'true')
+
+    await fetchCards();
+
+    if (debug === 'true')
         console.debug('There are', vCards.length, 'vCards.');
-}
+};
+
+export const getCards = async () => vCards.map(t => parseCards(t.data));
+
+/**
+ * Tries to get the card of a user that has `uid` as user data.
+ * @author Arnau Mora
+ * @since 20221104
+ * @param {string} uid The uid of the user.
+ * @return {Promise<PersonData|null>}
+ */
+export const getCard = async (uid) => {
+    const cards = await getCards();
+    return cards
+        .flat()
+        .find(v => v.uid === uid);
+};
