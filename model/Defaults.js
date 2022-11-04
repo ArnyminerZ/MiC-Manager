c3225cabbcff96cb2304d68ecb2323bc1828d236
Arnau Mora <arnyminer.z@gmail.com>
2022-11-04 22:08:28 +0100

    :sparkles: Added `people_see` permission
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- model/Defaults.js
+++ model/Defaults.js
@@ -18,6 +18,10 @@ const insertIfNotExists = (table, pairs) => {
 export const InsertDefaultRole = insertIfNotExists('mRoles', {Id: 1, DisplayName: 'DEFAULT'});
 
 export const InsertPermissions = [
+    // See all the tables for events
     insertIfNotExists('mPermissions', {Id: 1, DisplayName: 'tables_see'}),
+    // Add new events
     insertIfNotExists('mPermissions', {Id: 2, DisplayName: 'event_add'}),
+    // See all the registered people
+    insertIfNotExists('mPermissions', {Id: 3, DisplayName: 'people_see'}),
 ];
