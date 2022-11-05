505e6c4a0abd734bd42357401c0f944bfc365a62
Arnau Mora <arnyminer.z@gmail.com>
2022-11-05 16:06:42 +0100

    :sparkles: Added CLI utils
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ cli/logger.js
@@ -0,0 +1,17 @@
+import {BgBlue, BgGreen, BgRed, BgYellow, FgWhite, Reset} from './colors.js';
+
+export const error = (...objects) => {
+    console.error(BgRed + FgWhite + " FAIL " + Reset, ...objects)
+};
+
+export const warn = (...objects) => {
+    console.error(BgYellow + FgWhite + " WARN " + Reset, ...objects)
+};
+
+export const info = (...objects) => {
+    console.error(BgBlue + FgWhite + " INFO " + Reset, ...objects)
+};
+
+export const infoSuccess = (...objects) => {
+    console.error(BgGreen + FgWhite + "  OK  " + Reset, ...objects)
+};
