ff0a44f0e533e6bcfeab4716a81e138e759bc66b
Arnau Mora <arnyminer.z@gmail.com>
2022-11-05 18:45:25 +0100

    :sparkles: Added `/v1/migration/add_user`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/endpoints/migration.js
+++ src/endpoints/migration.js
@@ -1,6 +1,7 @@
-import {log, warn} from "../../cli/logger.js";
+import {error, log, warn} from "../../cli/logger.js";
 import {errorResponse, successResponse} from "../response.js";
 import {newCard} from "../request/caldav.js";
+import {newUser} from "../data/users.js";
 import {ParseException} from "../exceptions.js";
 
 export const addEndpoints = app => {
@@ -28,6 +29,29 @@ export const addEndpoints = app => {
                 res.status(401).json(errorResponse('invalid-request'));
             else
                 res.status(500).json(errorResponse(e));
+        }
+    });
+    app.post('/v1/migration/add_user', async (req, res) => {
+        const body = req.body;
+
+        /** @type {string|null} */
+        const dataRaw = body.data;
+        if (dataRaw == null) return res.status(400).json(errorResponse('missing-parameters'));
+
+        try {
+            /** @type {UserRow} */
+            const data = JSON.parse(dataRaw);
+
+            if (data.Uid == null || data.Role == null || data.Grade == null || data.WhitesWheel == null || data.BlacksWheel == null || data.NIF == null)
+                return res.status(401).json(errorResponse('invalid-request'));
+
+            const r = await newUser(data);
+            log('Add user:', r);
+
+            res.json(successResponse());
+        } catch (e) {
+            error('Could not add user. Error:', e);
+            res.status(500).json(errorResponse(e));
         }
     });
 };
