a0ac116f83ae0358f3450a6b82ac183ccd925351
Arnau Mora <arnyminer.z@gmail.com>
2022-11-17 20:50:36 +0100

    :white_check_mark: Added `/ping` and `/v1/info`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- test/api.js
+++ test/api.js
@@ -1,20 +1,15 @@
 // noinspection ES6UnusedImports
-import testcontainers, {DockerComposeEnvironment} from 'testcontainers';
-import http from 'http';
-import assert from 'assert';
+import testcontainers, {DockerComposeEnvironment, Wait} from 'testcontainers';
 import {faker} from '@faker-js/faker';
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import fs from "fs";
 
 const __dirname = process.env['NODE_PATH'];
 
-const request = (address) => new Promise((resolve, reject) => {
-    http.request(address, response => {
-        let str = '';
-
-        response.on('data', chunk => str += chunk);
-        response.on('end', () => resolve(str));
-        response.on('error', e => reject(e));
-    }).end();
-});
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 describe('API', function () {
     this.timeout(3 * 60000); // Timeout at 3 minutes
@@ -24,7 +19,7 @@ describe('API', function () {
     /** @type {string} */
     let dbUsername, dbDatabase;
     /** @type {string} */
-    let host, port, protocol;
+    let host, port, protocol, server;
 
     before('Generate parameters', () => {
         dbUsername = faker.internet.userName();
@@ -32,25 +27,53 @@ describe('API', function () {
     });
 
     before('Run Docker', async () => {
-        docker = await new DockerComposeEnvironment(__dirname, ['docker-compose.yml', 'docker-compose.override.yml'])
-            .withEnvironment({
-                DB_USERNAME: dbUsername,
-                DB_DATABASE: dbDatabase,
-                CALDAV_USERNAME: 'radicale',
-                CALDAV_PASSWORD: '',
-                CALDAV_AB_UUID: '',
-            })
-            .up();
+        const environment = new DockerComposeEnvironment(__dirname, ['docker-compose.yml', 'docker-compose.override.yml'])
+            .withBuild();
+        if (fs.existsSync('.env'))
+            docker = await environment
+                .withEnvironmentFile('.env')
+                .up();
+        else
+            docker = await environment
+                .withEnvironment({
+                    DB_USERNAME: dbUsername,
+                    DB_DATABASE: dbDatabase,
+                    CALDAV_USERNAME: 'radicale',
+                    CALDAV_PASSWORD: '',
+                    CALDAV_AB_UUID: '',
+                })
+                .up();
 
         const container = docker.getContainer('mic_interface');
         host = container.getHost();
         port = container.getMappedPort(3000).toString();
         protocol = 'http:';
+        server = `${protocol}//${host}:${port}`;
     });
 
-    it('Ping', async () => {
-        const r = await request({host, port, protocol, path: '/ping'});
-        assert.strictEqual(r, 'pong');
+    it('Ping (/ping)', (done) => {
+        chai.request(`${server}`)
+            .get('/ping')
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200).and.to.be.text;
+                expect(res.text).to.be.eql('pong');
+                done();
+            });
+    });
+    it('Information (/v1/info)', (done) => {
+        chai.request(`${server}`)
+            .get('/v1/info')
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200).and.to.be.json;
+                const body = res.body;
+                expect(body).to.have.property('success', true);
+                expect(body).to.have.property('data');
+                const data = body.data;
+                expect(data).to.have.property('database');
+                done();
+            });
     });
 
     after('Stop Docker', async () => {
