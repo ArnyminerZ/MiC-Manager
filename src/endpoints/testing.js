fba9bcf933e2d93aa76088d18346918d9c8dc56b
Arnau Mora <arnyminer.z@gmail.com>
2022-11-18 12:50:26 +0100

    :sparkles: Added `/v1/testing/drop_attempts`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/endpoints/testing.js
+++ src/endpoints/testing.js
@@ -1,5 +1,6 @@
 import {error, warn} from "../../cli/logger.js";
 import {newUser} from "../data/users.js";
+import {query} from "../request/database.js";
 
 export const addEndpoints = app => {
     warn('The "testing" prop is enabled. Do not keep enabled longer than needed.');
@@ -17,6 +18,16 @@ export const addEndpoints = app => {
                         : value // return everything else unchanged
                 ))
             });
+        } catch (e) {
+            error('Could not create new user. Error:', e);
+            res.status(500).json({error: e})
+        }
+    });
+
+    app.post('/v1/testing/drop_attempts', async (req, res) => {
+        try {
+            await query('DELETE FROM mLoginAttempts WHERE 1;');
+            res.json({success: true});
         } catch (e) {
             error('Could not create new user. Error:', e);
             res.status(500).json({error: e})
