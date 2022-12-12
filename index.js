b0aa7795a5838ee62ca7c115822d2847df5414f9
Arnau Mora <arnyminer.z@gmail.com>
2022-12-12 23:38:09 +0100

    Added bootup functions

--- index.js
+++ index.js
@@ -1,138 +1,15 @@
 'use strict';
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import reqIp from 'request-ip';
-import rateLimit from 'express-rate-limit';
-import {compare as compareVersion} from 'compare-versions';
-import https from 'https';
+import {bootCaldav, bootDatabase, bootEnvironment, bootFirefly, bootServer} from "./src/bootup.mjs";
 
-import {load as loadConfig} from './src/storage/config.js';
-loadConfig();
+bootEnvironment();
 
-import {errorResponse, successResponse} from './src/response.js';
-import {check as dbCheck, dbInfo as dbInfo} from './src/request/database.js';
-import {checkFiles, checkVariables, getProps} from './src/environment.js';
-import {createClient as calCreateClient, getAddressBookUrl, getCards} from "./src/request/caldav.js";
-import {error, info, infoSuccess, warn} from './cli/logger.js';
-import {addEndpoints as addMigrationEndpoints} from "./src/endpoints/migration.js";
-import {addEndpoints as addTestingEndpoints} from "./src/endpoints/testing.js";
-import {auth, changePassword, data, newUser} from "./src/endpoints/user.js";
-import {create, join, list, setMenu} from "./src/endpoints/events.js";
-import {check as checkFirefly} from './src/monetary/firefly.js';
-
-import packageJson from './package.json' assert {type: 'json'};
-import {decodeToken} from "./src/security.js";
-import {hasPermission} from "./src/permissions.js";
-import {SqlError} from "mariadb";
-import {checkPayments} from "./src/monetary/transactions.js";
-import {getLatestRelease} from "./src/info/release.js";
-
-checkVariables();
-checkFiles();
-const props = getProps();
-
-/**
- * The port number used for listening for http requests.
- * @type {number}
- */
-const HTTP_PORT = process.env.HTTP_PORT ?? 3000;
-
-info(`Checking database...`);
-const dbCheckResult = await dbCheck(process.env.LOG_LEVEL === 'debug');
-if (dbCheckResult != null) {
-    error(`Could not connect to database. Host: ${process.env.DB_HOSTNAME}`);
-    if (dbCheckResult instanceof SqlError)
-        error('Database error:', dbCheckResult.code, '-', dbCheckResult.text);
-    else
-        error('Error:', dbCheckResult);
-    process.exit(1);
-} else
-    infoSuccess(`Database connected.`);
-
-info(`Checking CalDAV server...`);
-if (!(await calCreateClient())) {
-    error(`Could not connect to the CalDAV server.`)
-    process.exit(1);
-}
-await getCards();
-infoSuccess(`CalDAV server ready. AB Url:`, getAddressBookUrl());
-
-info(`Checking Firefly server...`);
-await checkFirefly();
+await bootDatabase();
+await bootCaldav();
+await bootFirefly();
 
 // TODO: Fix stripe
 // info('Checking Stripe connection...');
 // await checkPayments();
 
-const app = express();
-
-// Limits the maximum amount of concurrent requests that can be made. If migration is enabled, the max rate is greatly
-// increased for allowing quick data write.
-const rateLimitOptions = {
-    windowMs: 60 * 1000,
-    max: props.includes('migration') || props.includes('testing') ? 500 : 10
-};
-const limiter = rateLimit(rateLimitOptions);
-info('Per minute rate limit:', rateLimitOptions.max);
-
-// Middleware
-app.use(reqIp.mw());
-app.use(bodyParser.json({strict: false}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(limiter);
-
-app.get('/ping', (req, res) => res.status(200).type('text/plain').send('pong'));
-app.get('/v1/info', async (req, res) => {
-    /** @type {string|null} */
-    const apiKey = req.get('API-Key');
-
-    try {
-        const database = await dbInfo();
-
-        let unauthorised = true;
-        if (apiKey != null) {
-            let tokenData;
-            try {
-                tokenData = await decodeToken(apiKey);
-            } catch (e) {
-                return res.status(400).send(errorResponse('invalid-key'));
-            }
-            if (!(await hasPermission(tokenData.userId, 'admin/version_view')))
-                return res.status(401).send(errorResponse('unauthorised'));
-            unauthorised = false;
-        }
-        if (unauthorised) return res.json(successResponse({database}));
-
-        const version = packageJson.version;
-        let latestRelease;
-        try {
-            latestRelease = await getLatestRelease();
-        }catch (err) {
-            error('Could not get latest version information. Error:', err);
-        }
-        const newVersion = latestRelease != null ? compareVersion(latestRelease, version, '>') : null;
-        res.json(successResponse({database, version: {name: version, update: newVersion}}));
-    } catch (e) {
-        error('Could not get version information. Error:', e);
-        return res.status(500).send(errorResponse(e instanceof Error ? e.toString() : e));
-    }
-});
-app.post('/v1/user/auth', auth);
-app.get('/v1/user/data', data);
-app.post('/v1/user/change_password', changePassword);
-app.put('/v1/user/new', newUser);
-app.get('/v1/events/list', list);
-app.post('/v1/events/create', create);
-app.post('/v1/events/:event_id/join', join);
-app.post('/v1/events/:event_id/set_menu', setMenu);
-
-// Extra endpoints
-if (props.includes('migration')) addMigrationEndpoints(app);
-if (props.includes('testing')) addTestingEndpoints(app);
-
-// Fallback
-app.get('*', (req, res) => res.status(404).json(errorResponse('invalid-request')));
-app.post('*', (req, res) => res.status(404).json(errorResponse('invalid-request')));
-
-app.listen(HTTP_PORT, () => info(`Listening for requests on http://localhost:${HTTP_PORT}`));
+bootServer();
---
+++ src/bootup.mjs
@@ -0,0 +1,108 @@
+/**
+ * Provides the functions that initialize the system, and create all the necessary files if any.
+ * @since 20221212
+ * @file bootup.mjs
+ */
+
+import {load as loadConfig} from "./storage/config.js";
+import {checkFiles, checkVariables, getProps} from "./environment.js";
+import {error, info, infoSuccess} from "../cli/logger.js";
+import {check as dbCheck} from "./request/database.js";
+import {SqlError} from "mariadb";
+import {createClient as calCreateClient, getAddressBookUrl, getCards} from "./request/caldav.js";
+import {check as checkFirefly} from "./monetary/firefly.js";
+import express from "express";
+import rateLimit from "express-rate-limit";
+import reqIp from "request-ip";
+import bodyParser from "body-parser";
+import {addEndpoints} from "./endpoints/add_endpoints.mjs";
+
+/**
+ * Boots all the environment-related features. Those include necessary files, configuration, variables...
+ * @author Arnau Mora
+ * @since 20221212
+ */
+export const bootEnvironment = () => {
+    loadConfig();
+    checkVariables();
+    checkFiles();
+};
+
+/**
+ * Boots and connects to the database, adding all the necessary data, running all migrations, and checking that
+ * everything is up and running.
+ * @author Arnau Mora
+ * @since 20221212
+ * @returns {Promise<void>}
+ */
+export const bootDatabase = async () => {
+    info(`Checking database...`);
+    const dbCheckResult = await dbCheck(process.env.LOG_LEVEL === 'debug');
+    if (dbCheckResult != null) {
+        error(`Could not connect to database. Host: ${process.env.DB_HOSTNAME}`);
+        if (dbCheckResult instanceof SqlError)
+            error('Database error:', dbCheckResult.code, '-', dbCheckResult.text);
+        else
+            error('Error:', dbCheckResult);
+        process.exit(1);
+    } else
+        infoSuccess(`Database connected.`);
+};
+
+/**
+ * Checks that the CalDAV server is up and running correctly, and creates any necessary collections if necessary.
+ * @author Arnau Mora
+ * @since 20221212
+ * @returns {Promise<void>}
+ */
+export const bootCaldav = async () => {
+    info(`Checking CalDAV server...`);
+    if (!(await calCreateClient())) {
+        error(`Could not connect to the CalDAV server.`)
+        process.exit(1);
+    }
+    await getCards();
+    infoSuccess(`CalDAV server ready. AB Url:`, getAddressBookUrl());
+};
+
+/**
+ * Checks that the Firefly backend is running correctly, and initializes it if not done yet.
+ * @author Arnau Mora
+ * @since 20221212
+ * @returns {Promise<void>}
+ */
+export const bootFirefly = async () => {
+    info(`Checking Firefly server...`);
+    await checkFirefly();
+};
+
+/**
+ * Boots the HTTP server, and adds all the endpoints necessary, including the provided by the different props. Also adds
+ * all the necessary middleware.
+ * @author Arnau Mora
+ * @since 20221212
+ */
+export const bootServer = () => {
+    const props = getProps();
+
+    const app = express();
+
+    // Limits the maximum amount of concurrent requests that can be made. If migration is enabled, the max rate is greatly
+    // increased for allowing quick data write.
+    const rateLimitOptions = {
+        windowMs: 60 * 1000,
+        max: props.includes('migration') || props.includes('testing') ? 500 : 10
+    };
+    const limiter = rateLimit(rateLimitOptions);
+    info('Per minute rate limit:', rateLimitOptions.max);
+
+    // Middleware
+    app.use(reqIp.mw());
+    app.use(bodyParser.json({strict: false}));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(limiter);
+
+    addEndpoints(app, props);
+
+    app.listen(process.env.HTTP_PORT, () => info(`Listening for requests on http://localhost:${process.env.HTTP_PORT}`));
+};
