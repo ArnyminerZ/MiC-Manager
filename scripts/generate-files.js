b3b7c714784c7e3456b1809517eb7370efbd9989
Arnau Mora <arnyminer.z@gmail.com>
2022-12-12 23:41:20 +0100

    Added `__dirname` import

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -1,5 +1,6 @@
 import {generateSecrets} from "./generator.js";
 import path from "path";
+import {__dirname} from "../src/utils.mjs";
 
 const secretsDir = path.join(__dirname, 'secrets');
 generateSecrets(secretsDir);
