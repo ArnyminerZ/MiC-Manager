f2cef5a6337b678bece7c69e55268511f803173c
Arnau Mora <arnyminer.z@gmail.com>
2022-12-13 19:55:49 +0100

    Added branding initialization

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import {bootCaldav, bootDatabase, bootEnvironment, bootFirefly, bootServer} from "./src/bootup.mjs";
+import {initializeBranding} from "./src/endpoints/branding.mjs";
 
 bootEnvironment();
 
 await bootDatabase();
 await bootCaldav();
 await bootFirefly();
+
+await initializeBranding();
 
 // TODO: Fix stripe
 // info('Checking Stripe connection...');
