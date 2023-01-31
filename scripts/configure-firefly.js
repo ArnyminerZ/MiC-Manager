7f0bdc2c23480920bafeb5f8f5f58113ab6094dd
Arnau Mora <arnyminer.z@gmail.com>
2023-01-31 12:13:46 +0100

    Firefly email and password is generated in generator
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- scripts/configure-firefly.js
+++ scripts/configure-firefly.js
@@ -1,5 +1,4 @@
 import path from "path";
-import {faker} from "@faker-js/faker";
 
 import {__dirname} from "../src/utils.mjs";
 import {configure as configureFirefly} from "../src/monetary/firefly.js";
@@ -8,12 +7,7 @@ import {error} from "../cli/logger.js";
 const secretsDir = path.join(__dirname, 'secrets');
 
 try {
-    await configureFirefly(
-        process.env.FIREFLY_EMAIL ?? faker.internet.email(),
-        process.env.FIREFLY_PASSWORD ?? faker.internet.password(32),
-        secretsDir,
-        process.env.SCREENSHOTS_DIR,
-    );
+    await configureFirefly(null, null, secretsDir, process.env.SCREENSHOTS_DIR);
 } catch (e) {
     error('Could not configure Firefly. Error:', e);
 }
--- scripts/generator.js
+++ scripts/generator.js
@@ -2,6 +2,8 @@ import fs from "fs";
 import path from "path";
 import {info} from "../cli/logger.js";
 
+const AlphaNum = [[48, 57], [65, 90], [97, 122]];
+
 /**
  * Generates a random integer from the given interval (both inclusively).
  * @param {number} min The minimum number to generate.
@@ -11,22 +13,56 @@ import {info} from "../cli/logger.js";
 const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 /**
+ * Generates a random integer from the given intervals.
+ * @param {[number,number]} intervals
+ * @return {number}
+ */
+const randomIntFromIntervals = (...intervals) => {
+    const intervalIndex = randomIntFromInterval(0, intervals.length - 1);
+    const interval = intervals[intervalIndex];
+    return randomIntFromInterval(interval[0], interval[1]);
+}
+
+/**
  * Generates a random string with the given length.
- * @param {number} length
+ *
+ * Uses ASCII characters from the set `from`-`to`.
+ * @param {number} length The length of the generated string.
+ * @param {[start:number,end:number][]} intervals The intervals of characters to be used. Uses ASCII codes.
  * @returns {string}
  */
-const generator = (length) => [...(new Array(length))].map(() => String.fromCharCode(randomIntFromInterval(33, 126))).join('');
+export const generator = (length, intervals = [[33, 126]]) =>
+    [...(new Array(length))].map(() => String.fromCharCode(randomIntFromIntervals(...intervals))).join('');
 
 export const generateSecrets = secretsDir => {
     if (!fs.existsSync(secretsDir)) fs.mkdirSync(secretsDir);
 
-    const secrets = ['password', 'root-password', 'firefly-app-key', 'database', 'username'];
+    const passwordSecrets = ['password', 'root-password', 'firefly-app-key', 'firefly-user-password'];
+    const alphaNumSecrets = ['database', 'username'];
+    const emailSecrets = ['firefly-user-email'];
 
-    for (const secret of secrets) {
+    for (const secret of passwordSecrets) {
         const file = path.join(secretsDir, `${secret}.txt`);
         if (!fs.existsSync(file)) {
             info('Generating random contents for', file);
             fs.writeFileSync(file, generator(32));
+        }
+    }
+    for (const secret of alphaNumSecrets) {
+        const file = path.join(secretsDir, `${secret}.txt`);
+        if (!fs.existsSync(file)) {
+            info('Generating random contents for', file);
+            fs.writeFileSync(file, generator(32, AlphaNum));
+        }
+    }
+    for (const secret of emailSecrets) {
+        const file = path.join(secretsDir, `${secret}.txt`);
+        if (!fs.existsSync(file)) {
+            info('Generating random contents for', file);
+            fs.writeFileSync(
+                file,
+                `${generator(32, AlphaNum)}@${generator(5, AlphaNum)}.com`,
+            );
         }
     }
 
