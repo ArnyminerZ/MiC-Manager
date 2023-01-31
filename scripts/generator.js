65aca48a68a38854381f90b8322b986d50db3ab3
Arnau Mora <arnyminer.z@gmail.com>
2023-01-31 09:39:18 +0100

    Faker no longer needed
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,24 +1,38 @@
 import fs from "fs";
 import path from "path";
 import {info} from "../cli/logger.js";
-import {faker} from "@faker-js/faker";
+
+/**
+ * Generates a random integer from the given interval (both inclusively).
+ * @param {number} min The minimum number to generate.
+ * @param {number} max The maximum number to generate.
+ * @returns {number}
+ */
+const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+/**
+ * Generates a random string with the given length.
+ * @param {number} length
+ * @returns {string}
+ */
+const generator = (length) => [...(new Array(length))].map(() => String.fromCharCode(randomIntFromInterval(33, 126))).join('');
 
 export const generateSecrets = secretsDir => {
     if (!fs.existsSync(secretsDir)) fs.mkdirSync(secretsDir);
 
-    const secrets = ['password', 'root-password', 'firefly-password', 'firefly-root-password', 'firefly-app-key'];
+    const secrets = ['password', 'root-password', 'firefly-app-key', 'database', 'username'];
 
     for (const secret of secrets) {
         const file = path.join(secretsDir, `${secret}.txt`);
         if (!fs.existsSync(file)) {
             info('Generating random contents for', file);
-            fs.writeFileSync(file, faker.internet.password(32));
+            fs.writeFileSync(file, generator(32));
         }
     }
 
     const privateKey = path.join(secretsDir, 'private.key');
     if (!fs.existsSync(privateKey)) {
         info('Generating random contents for', privateKey);
-        fs.writeFileSync(privateKey, faker.random.alphaNumeric(1024));
+        fs.writeFileSync(privateKey, generator(1024));
     }
 };
