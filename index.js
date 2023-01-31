a57c3b8956c5d0805bf46816c6546f34a2b81f62
Arnau Mora <arnyminer.z@gmail.com>
2023-01-31 12:15:41 +0100

    Added title
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 
+import packageJson from "./package.json" assert {type: 'json'};
 import {bootCaldav, bootDatabase, bootEnvironment, bootFirefly, bootServer} from "./src/bootup.mjs";
 import {initializeBranding} from "./src/endpoints/branding.mjs";
+import {info} from "./cli/logger.js";
+
+info('');
+info('  __  __ _  _____   __  __                                   ');
+info(' |  \\/  (_)/ ____| |  \\/  |                                  ');
+info(' | \\  / |_| |      | \\  / | __ _ _ __   __ _  __ _  ___ _ __ ');
+info(' | |\\/| | | |      | |\\/| |/ _` | \'_ \\ / _` |/ _` |/ _ \\ \'__|');
+info(' | |  | | | |____  | |  | | (_| | | | | (_| | (_| |  __/ |   ');
+info(' |_|  |_|_|\\_____| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   ');
+info('                                              __/ |          ');
+info('                                             |___/           ');
+info('');
+info('Version:', packageJson.version);
+info('');
 
 bootEnvironment();
 
