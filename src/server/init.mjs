38a227fa8f7bd6606113b81887f6f4f1d30c94bc
Arnau Mora <arnyminer.z@gmail.com>
2023-02-08 09:24:14 +0100

    Added rate limiter
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/server/init.mjs
+++ src/server/init.mjs
@@ -1,4 +1,6 @@
 import express from 'express';
+import RateLimit from 'express-rate-limit';
+
 import {info, log} from '../../cli/logger.mjs';
 import {pingEndpoint} from './endpoints/utils.mjs';
 import {loginEndpoint, registerEndpoint} from "./endpoints/auth.mjs";
@@ -11,8 +13,16 @@ export const initServer = () => {
     info('Initializing web server...');
     const app = express();
 
+
     log('Adding middlewares...');
     app.use(express.json());
+
+    const limiter = new RateLimit({
+        windowMs: 60*1000, // 1 minute
+        max: 5
+    });
+    // noinspection JSCheckFunctionSignatures
+    app.use(limiter);
 
     app.get('/', (req, res) => res.status(200).send('ok'));
     app.get('/v1/ping', pingEndpoint);
