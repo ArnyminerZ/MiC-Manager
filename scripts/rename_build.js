cd6d329119733a72dfdf3c2850ca486f2a028652
Arnau Mora <arnyminer.z@gmail.com>
2023-02-15 22:59:33 +0100

    Added js extension appending
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ scripts/rename_build.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(path.join(__filename, '..'));
+const build = path.join(__dirname, 'build');
+if (!fs.existsSync(build)) {
+    console.warn('Build directory doesn\'t exist');
+    process.exit(1);
+}
+
+const importRegex = /import .* from ["'].*["'];?/gm;
+
+/**
+ * Renames all the `.js` files in `path` to `.mjs`.
+ * @param {string} dir The location of the files in the filesystem.
+ * @param {number} depth The depth of the current iteration.
+ */
+function renameDir(dir, depth = 0) {
+    /** @type {string[]} */
+    const files = fs.readdirSync(dir).map(file => path.join(dir, file));
+    for (const file of files) {
+        const stat = fs.lstatSync(file);
+        // console.log(' '.repeat(depth) + "-", file);
+        if (stat.isDirectory())
+            renameDir(file, depth + 1);
+        else {
+            if (!file.endsWith('js')) continue;
+
+            const contents = fs.readFileSync(file).toString();
+            let updatedContents = contents;
+
+            let myArray;
+            while ((myArray = importRegex.exec(contents)) !== null) {
+                let line = myArray[0];
+                if (!line.includes('./')) continue;
+                if (line.includes('js')) continue;
+                if (!line.trimEnd().endsWith(';')) line += ';';
+                const pos = line.length - 2;
+                const newLine = line.substring(0, pos) + '.js' + line.substring(pos);
+
+                console.log(' '.repeat(depth) + "-", myArray[0], '->', newLine);
+                updatedContents = updatedContents.replace(myArray[0], newLine);
+            }
+            console.log(' '.repeat(depth) + "-", file);
+            fs.rmSync(file);
+            fs.writeFileSync(file, updatedContents);
+        }
+    }
+}
+
+renameDir(build);
